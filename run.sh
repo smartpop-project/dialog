843f3f1f18b0b699b4e5e8667002408b4fffdda6
luke <luke.yang@belivvr.com>
2025-01-10 17:36:27 +0000

    io_uring problem solving

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ MEDIASOUP_ANNOUNCED_IP=$(curl "https://ipinfo.io/ip")
 # 그러나 포트 맵핑을 하면 시간이 오래 걸린다.
 # 그리고 사용 중인 포트가 있을 가능성이 높다.
 # 그래서 실행에 실패하다.
-docker run --log-opt max-size=10m --log-opt max-file=3 -d --restart=always --name dialog \
+docker run --privileged --log-opt max-size=10m --log-opt max-file=3 -d --restart=always --name dialog \
 --network="host" \
 -v $(pwd)/certs/cert.pem:/app/certs/fullchain.pem \
 -v $(pwd)/certs/key.pem:/app/certs/privkey.pem \
@@ -21,6 +21,7 @@ docker run --log-opt max-size=10m --log-opt max-file=3 -d --restart=always --nam
 -e MEDIASOUP_LISTEN_IP="0.0.0.0" \
 -e MEDIASOUP_ANNOUNCED_IP=${MEDIASOUP_ANNOUNCED_IP} \
 -e INTERACTIVE="false" \
+--cap-add=SYS_ADMIN \
 dialog
 
 docker logs dialog
